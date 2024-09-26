0a87c6cfd45b0b9d5cd2292d99e192e932076903
Sqrl28 <devonsapps@proton.me>
2024-09-26 00:04:30 -0700

    Added Files

---
+++ software/raspberrypi/test_command.sh
@@ -0,0 +1,5 @@
+### Test Code
+
+tom@raspberrypi:~ $ cd cybernetic-stream-table/
+tom@raspberrypi:~/cybernetic-stream-table $ cat ssh_commands.sh
+tom@raspberrypi:~.cybernetic-stream-table $ libcamera-jpeg --qt-preview on -o test.jpg
