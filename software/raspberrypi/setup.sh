f9057a974a17a868a163ee9527fad737f185b544
bill-mca <w.mcalister@gmx.com>
2024-10-14 15:49:49 +1100

    Comments on further development

--- software/raspberrypi/setup.sh
+++ software/raspberrypi/setup.sh
@@ -1,5 +1,8 @@
-python3 -m venv ./venv
-source ./venv/bin/activate
+python3 -m venv ./.venv
+source ./.venv/bin/activate
 
+# Note that requirements.txt currently contains all the necessary
+# modules to train the models. We need to make a simpler version just 
+# for running the model. it is nearly a GB at the moment.
 pip3 install -r requirements.txt
 
