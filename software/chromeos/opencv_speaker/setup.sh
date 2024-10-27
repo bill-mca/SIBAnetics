f7c06b57f7ee016fc9483dbaa763779396f694dd
bill-mca <w.mcalister@gmx.com>
2024-10-28 10:55:19 +1100

    added a line to install cmake.

--- software/chromeos/opencv_speaker/setup.sh
+++ software/chromeos/opencv_speaker/setup.sh
@@ -1,10 +1,13 @@
 #python3 -m venv ./venv
+
+sudo apt install cmake
+
 ./venv/bin/pip3 install -r requirements.txt
 
-# wget https://github.com/spmallick/learnopencv/raw/refs/heads/master/FaceDetectionComparison/models/res10_300x300_ssd_iter_140000_fp16.caffemodel
+wget https://github.com/spmallick/learnopencv/raw/refs/heads/master/FaceDetectionComparison/models/res10_300x300_ssd_iter_140000_fp16.caffemodel
 
-# wget https://raw.githubusercontent.com/spmallick/learnopencv/refs/heads/master/FaceDetectionComparison/models/deploy.prototxt
+wget https://raw.githubusercontent.com/spmallick/learnopencv/refs/heads/master/FaceDetectionComparison/models/deploy.prototxt
 
-#wget https://raw.githubusercontent.com/spmallick/learnopencv/refs/heads/master/FaceDetectionComparison/models/haarcascade_frontalface_default.xml
+wget https://raw.githubusercontent.com/spmallick/learnopencv/refs/heads/master/FaceDetectionComparison/models/haarcascade_frontalface_default.xml
 
 wget https://raw.githubusercontent.com/atduskgreg/opencv-processing/refs/heads/master/lib/cascade-files/haarcascade_mcs_mouth.xml
